c9b824992f1486d04c85bf5299192816e016ed9b
hetong007 <hetong007@gmail.com>
2015-03-18 16:28:57 -0700

    add plda

--- code/xgboost.R
+++ code/xgboost.R
@@ -17,15 +17,15 @@ mlogloss <- function(preds, dtrain) {
 
 thread = 8
 param <- list("objective" = "multi:softprob",
-              "bst:eta" = 0.003,
-              "bst:max_depth" = 12,
+              "bst:eta" = 0.005,
+              "bst:max_depth" = 10,
               "gamma" = 1,
               "eval_metric" = "merror",
               "silent" = 1,
-              "min_child_weight" = 50,
+              "min_child_weight" = 100,
               "subsample" = 0.75,
               "num_class" = 9,
-              "colsample_bytree" = 0.8,
+              "colsample_bytree" = 0.5,
               "nthread" = thread)
 cv.nround = 5000
 
