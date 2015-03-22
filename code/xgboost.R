cb51d361195624186454c21c9ac12260c3f43f64
Ubuntu <ubuntu@ip-10-90-4-68.us-west-2.compute.internal>
2015-03-22 21:23:56 +0000

    stronger parameters

--- code/xgboost.R
+++ code/xgboost.R
@@ -5,7 +5,7 @@ y = y-1 # xgboost take features in [0,numOfClass)
 
 thread = 16
 param <- list("objective" = "multi:softprob",
-              "bst:eta" = 0.05,
+              "bst:eta" = 0.005,
               "bst:max_depth" = 20,
               "gamma" = 1,
               "eval_metric" = "mlogloss",
@@ -13,9 +13,9 @@ param <- list("objective" = "multi:softprob",
               "min_child_weight" = 50,
               "subsample" = 0.8,
               "num_class" = 9,
-              "colsample_bytree" = 0.8,
+              "colsample_bytree" = 0.9,
               "nthread" = thread)
-cv.nround = 1500
+cv.nround = 8000
 
 # Cross Validation
 bst.cv = xgb.cv(param=param, data = x[trind,], label = y, 
@@ -28,7 +28,7 @@ lines(bst.cv[,3],col=2)
 valid_uplim = bst.cv[,3]+bst.cv[,4]
 nround = which.min(valid_uplim)
 Pred = matrix(0,length(teind),9)
-num_bag = 10
+num_bag = 20
 for (i in 1:num_bag)
 {
     ind = sample(trind)
